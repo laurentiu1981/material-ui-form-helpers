b42f767833a17f3251b0e1a2cfcbe34dea4e0de4
Laurentiu Lese <laurentiu.lese@freshbyteinc.com>
2019-08-16 03:19:11 +0300

    Add dev build.

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist/'),
     publicPath: '',
-    filename: 'matuihelpers.js',
+    filename: 'index.js',
     libraryTarget: 'umd',
   },
 };
