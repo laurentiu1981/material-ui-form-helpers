ab23bcf55d1d6c251c7a3ea8a580fda8ad548291
Laurentiu Lese <laurentiu.lese@freshbyteinc.com>
2019-12-06 16:10:34 +0200

    Expose LoadingBox component

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import FieldSelect from './FieldSelect';
 import SubmitButton from './SubmitButton';
 import Table from './Table';
 import MultiValueSelect from './MultiValueSelect';
+import LoadingBox from './LoadingBox';
 
-export { FieldCheckbox, FieldSelect, Table, FieldColor, SubmitButton, MultiValueSelect };
+export { FieldCheckbox, FieldSelect, Table, FieldColor, SubmitButton, MultiValueSelect, LoadingBox };
